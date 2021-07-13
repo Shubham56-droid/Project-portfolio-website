e3eb98f956999070af645eb7e88d518b57f638f0
Shubham Bawankar <71594948+Shubham56-droid@users.noreply.github.com>
2021-07-14 00:45:26 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const navToggler = document.querySelector('.nav-toggler');
 navToggler.addEventListener("click",()=>{
   
     hideSection();
-
     toggleNavbar();
+    document.body.classList.toggle("hide-scrolling");
     
 });
 function toggleNavbar()
@@ -19,7 +19,31 @@ function hideSection()
   document.querySelector("section.active").classList.toggle("fade-out");
 }
 
-
+/*----------------Active Section--------------------*/
+document.addEventListener("click",(e)=>{
+  if(e.target.classList.contains("link-item") && e.target.hash !== "")
+  {
+    const hash = e.target.hash;
+    navToggler.classList.add("hide");
+    if(e.target.classList.contains("nav-item"))
+    {
+        toggleNavbar();
+    }
+    else
+    {
+      hideSection();
+      document.body.classList.add("hide-scrolling");
+    }
+    setTimeout(()=>{
+      document.querySelector("section.active").classList.remove("active","fade-out");
+      document.querySelector(e.target.hash).classList.add("active");
+      window.scrollTo(0,0);
+      document.body.classList.remove("hide-scrolling");
+      navToggler.classList.remove("hide");
+    },500)
+  }
+})
+/*---------------About files------------------------*/
 
 /*-------------------JavaScript for About Tabs------------------*/
 const tabsContainer = document.querySelector(".about-tabs");
